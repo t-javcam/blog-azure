839dbb4a68feaa444688e15318cf61d857164077
Javier Campanini <jmcampanini@gmail.com>
2012-08-02 17:31:18 -0700

    fix

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,4 +1,4 @@
-root = "/home/blogger/apps/blog/current"
+root = "/home/blogger/apps/blog_app/current"
 working_directory root
 pid "#{root}/tmp/pids/unicorn.pid"
 stderr_path "#{root}/log/unicorn.log"
